4e0e8c23472a18acbca9cc7a9d8815b1b27e83e4
pudilz <zack.pudil@dowjones.com>
2017-04-26 15:59:10 -0500

    Added all pieces to the move engine.

--- src/chess/move-engine.js
+++ src/chess/move-engine.js
@@ -1,6 +1,8 @@
 import { WHITE, BLACK } from '../modules/pieces';
 import { letterToNumber, numberToLetter } from './board';
 
+const filterBadSquares = (squares) => squares.filter(s => !(s.x > 8 || s.x < 1 || s.y > 8 || s.y < 1));
+
 export const pawn = (x, y, white) => {
 	if(white) {
 		if(y == 2) return [{ x, y: y + 1 }, { x, y: y + 2 }];
@@ -25,7 +27,49 @@ export const knight = (x, y) => {
 		{ x: x + 2, y: y - 1 }
 	];
 
-	return squares.filter(s => !(s.x > 8 || s.x < 1 || s.y > 8 || s.y < 1));
+	return filterBadSquares(squares);
+};
+
+export const bishop = (x, y) => {
+	let squares = [];
+	for(let i = 1; i <= 8; i++) {
+		squares.push({ x: x - i, y: y - i });
+		squares.push({ x: x + i, y: y - i });
+		squares.push({ x: x + i, y: y + i });
+		squares.push({ x: x - i, y: y + i });
+	}
+	return filterBadSquares(squares);
+};
+
+export const rook = (x, y) => {
+	let squares = [];
+	for(let i = 1; i <= 8; i++) {
+		squares.push({x, y: y - i});
+		squares.push({x, y: y + i});
+		squares.push({x: x + i, y});
+		squares.push({x: x - i, y});
+	}
+
+	return filterBadSquares(squares);
+}
+
+export const queen = (x, y) => {
+	return rook(x, y).concat(bishop(x, y));
+};
+
+export const king = (x, y) => {
+	let squares = [
+		{ x, y: y + 1 },
+		{ x, y: y - 1 },
+		{ x: x - 1, y },
+		{ x: x + 1, y },
+		{ x: x - 1, y: y - 1 },
+		{ x: x - 1, y: y + 1 },
+		{ x: x + 1, y: y - 1 },
+		{ x: x + 1, y: y + 1 }
+	];
+
+	return filterBadSquares(squares);
 };
 
 const squareToCoords = (calc, color) => (square) => {
@@ -41,5 +85,13 @@ export default {
 	'P': squareToCoords(pawn, WHITE),
 	'p': squareToCoords(pawn, BLACK),
 	'N': squareToCoords(knight, WHITE),
-	'n': squareToCoords(knight, BLACK)
+	'n': squareToCoords(knight, BLACK),
+	'B': squareToCoords(bishop, WHITE),
+	'b': squareToCoords(bishop, BLACK),
+	'R': squareToCoords(rook, WHITE),
+	'r': squareToCoords(rook, BLACK),
+	'Q': squareToCoords(queen, WHITE),
+	'q': squareToCoords(queen, BLACK),
+	'K': squareToCoords(king, WHITE),
+	'k': squareToCoords(king, BLACK)
 };
--- src/chess/move-engine.test.js
+++ src/chess/move-engine.test.js
@@ -52,4 +52,67 @@ describe('moveEngine', () => {
 			assert.deepEqual(test2.sort(), ['f3', 'h3', 'e2'].sort());
 		});
 	});
+
+	describe('biship', () => {
+		it('should exist.', () => assert(subject.b && subject.B));
+
+		it('should move diagnal', () => {
+			let test = subject.b('f3');
+			let test2 = subject.B('f3');
+
+			assert.deepEqual(test.sort(),
+				[ 'e2', 'g2', 'g4', 'e4', 'd1', 'h1', 'h5', 'd5', 'c6', 'b7', 'a8' ].sort());
+			assert.deepEqual(test2.sort(),
+				[ 'e2', 'g2', 'g4', 'e4', 'd1', 'h1', 'h5', 'd5', 'c6', 'b7', 'a8' ].sort());
+		});
+	});
+
+	describe('root', () => {
+		it('should exist.', () => assert(subject.r && subject.R));
+
+		it('should move latterally.', () => {
+			let test = subject.r('f3');
+			let test2 = subject.R('f3');
+
+			assert.deepEqual(test.sort(),
+				[ 'f2','f4','g3','e3','f1','f5','h3','d3','f6','c3','f7','b3','f8','a3' ].sort())
+
+			assert.deepEqual(test2.sort(),
+				[ 'f2','f4','g3','e3','f1','f5','h3','d3','f6','c3','f7','b3','f8','a3' ].sort())
+		});
+	});
+
+	describe('king', () => {
+		it('should exist.', () => assert(subject.k && subject.K));
+
+		it('should move one square in all directions.', () => {
+			let test = subject.k('f3');
+			let test2 = subject.K('f3');
+
+			assert.deepEqual(test.sort(),
+				['f2', 'f4', 'g2', 'g3', 'g4', 'e2', 'e3', 'e4'].sort());
+			assert.deepEqual(test2.sort(),
+				['f2', 'f4', 'g2', 'g3', 'g4', 'e2', 'e3', 'e4'].sort());
+
+		});
+	});
+
+	describe('queen', () => {
+		it('should exist.', () => assert(subject.q && subject.Q));
+
+		it('should move like a bishop and rook together.', () => {
+			let test = subject.q('f3');
+			let test2 = subject.Q('f3');
+
+			assert.deepEqual(test.sort(),
+				[ 'a3','a8','b3','b7','c3','c6','d1','d3','d5',
+					'e2','e3','e4','f1','f2','f4','f5','f6','f7',
+					'f8','g2','g3','g4','h1','h3','h5' ].sort());
+
+			assert.deepEqual(test2.sort(),
+				[ 'a3','a8','b3','b7','c3','c6','d1','d3','d5',
+					'e2','e3','e4','f1','f2','f4','f5','f6','f7',
+					'f8','g2','g3','g4','h1','h3','h5' ].sort());
+		});
+	});
 });
