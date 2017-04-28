c3c1e4efeca91c338aa0ff996d7563504eefb283
zackpudil <zack.pudil@gmail.com>
2017-04-28 12:38:02 -0500

    Moved square and piece components into seperate file

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -2,36 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {initSquares} from '../modules/squares';
 import {routePiece, movePiece, BLACK, NO_PIECE_ID} from '../modules/pieces';
-
-export const Piece = (props) => {
-  let pieceClass = props.pieceId === NO_PIECE_ID 
-    ? "" : props.color === BLACK ? 'black' : 'white';
-
-  return (
-    <a href="javascript:void(0)" className={pieceClass}
-      onClick={props.pieceClick}>{props.pieceId}</a>
-  );
-};
-
-export const Square = (props) => {
-	let pieceClick = () => {
-		if(props.pieceId === NO_PIECE_ID)
-			props.movePiece(props.id);
-		else
-			props.routePiece(props.id, props.pieceId);
-	};
-
-  let squareClass = props.highlighted 
-    ? props.pieceId === NO_PIECE_ID ? "highlighted" : "highlighted-red" 
-    : "";
-	squareClass += props.selected ? "selected" : "";
-
-	return (
-		<div className={squareClass}> 
-      <Piece pieceId={props.pieceId} color={props.color}  pieceClick={pieceClick} />
-		</div>
-	);
-};
+import {Square} from './Square';
 
 class Board extends React.Component {
 	componentDidMount() {
---
+++ src/components/square.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {BLACK, NO_PIECE_ID} from '../modules/pieces';
+
+export const Piece = (props) => {
+  let pieceClass = props.pieceId === NO_PIECE_ID 
+    ? "" : props.color === BLACK ? 'black' : 'white';
+
+  return (
+    <a href="javascript:void(0)" className={pieceClass}
+      onClick={props.pieceClick}>{props.pieceId}</a>
+  );
+};
+
+export const Square = (props) => {
+	let pieceClick = () => {
+		if(props.pieceId === NO_PIECE_ID)
+			props.movePiece(props.id);
+		else
+			props.routePiece(props.id, props.pieceId);
+	};
+
+  let squareClass = props.highlighted 
+    ? props.pieceId === NO_PIECE_ID ? "highlighted" : "highlighted-red" 
+    : "";
+	squareClass += props.selected ? "selected" : "";
+
+	return (
+		<div className={squareClass}> 
+      <Piece pieceId={props.pieceId} color={props.color}  pieceClick={pieceClick} />
+		</div>
+	);
+};
+
