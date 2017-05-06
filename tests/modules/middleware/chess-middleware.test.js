9d799a8fd6ee219bc39c40e1454373e1d2c44829
zackpudil <zack.pudil@gmail.com>
2017-05-06 15:47:50 -0500

    Small white space.

--- tests/modules/middleware/chess-middleware.test.js
+++ tests/modules/middleware/chess-middleware.test.js
@@ -21,9 +21,7 @@ describe('Chess middleware', () => {
 
 	describe('Route action', () => {
 		beforeEach(() => {
-			sinon.stub(chess, 'default').returns(
-				(p) => () => ['a1', 'b1', 'c4']
-      ); 
+			sinon.stub(chess, 'default').returns((p) => () => ['a1', 'b1', 'c4']); 
 			sinon.spy(squares, 'highlightSquare');
 			sinon.spy(squares, 'selectSquare');
 			sinon.spy(squares, 'clearHighlights');
